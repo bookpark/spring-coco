eebb247df06ac0620fae17d82ac5adf31c72436a
bookpark <bkbkgg@gmail.com>
2022-12-05 01:54:14 +0900

    Member jpa repository 추가

---
+++ src/main/java/kfq/springcoco/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package kfq.springcoco.repository;
+
+import kfq.springcoco.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+}
