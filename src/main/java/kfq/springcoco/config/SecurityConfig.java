a74fff05e688b8446a18b1f31200bd1dcabdac99
bookpark <bkbkgg@gmail.com>
2022-12-06 20:48:30 +0900

    모든 url에 대한 접근 열기

--- src/main/java/kfq/springcoco/config/SecurityConfig.java
+++ src/main/java/kfq/springcoco/config/SecurityConfig.java
@@ -2,16 +2,29 @@ package kfq.springcoco.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 인증되지 않아도 모든 요청 허락
+        http.authorizeHttpRequests().antMatchers("/**").permitAll();
+        return http.build();
+    }
 }
+
+
