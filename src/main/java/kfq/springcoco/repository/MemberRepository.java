de664660e042904383d488615e29c2748336eb46
bookpark <bkbkgg@gmail.com>
2022-12-06 13:35:17 +0900

    닉네임, 이메일 별로 찾아주는 메서드 추가

--- src/main/java/kfq/springcoco/repository/MemberRepository.java
+++ src/main/java/kfq/springcoco/repository/MemberRepository.java
@@ -4,6 +4,15 @@ import kfq.springcoco.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByEmail(String email);
+
+    Boolean existsByNickname(String nickname);
+
+    Boolean existsByEmail(String email);
 }
