f91cf8b0df47d5fdd23ab8c6e591bd262612b95e
bookpark <bkbkgg@gmail.com>
2022-12-07 12:58:21 +0900

    회원가입 api 작성

---
+++ src/main/java/kfq/springcoco/controller/MemberController.java
@@ -0,0 +1,33 @@
+package kfq.springcoco.controller;
+
+import kfq.springcoco.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/members")
+    public ResponseEntity<String> signUp(@RequestParam("email") String email,
+                                         @RequestParam("password") String password,
+                                         @RequestParam("nickname") String nickname
+    ) {
+        ResponseEntity<String> res = null;
+        try {
+            memberService.signUp(email, password, nickname);
+            res = new ResponseEntity<String>("질문 작성 성공", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            res = new ResponseEntity<String>("질문 작성 실패", HttpStatus.BAD_REQUEST);
+        }
+        return res;
+    }
+
+}
