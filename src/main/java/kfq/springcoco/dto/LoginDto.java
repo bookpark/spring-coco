949fa21c917212297d5d117fe0f247f509a67187
bookpark <bkbkgg@gmail.com>
2022-12-07 14:47:41 +0900

    dto 추가

---
+++ src/main/java/kfq/springcoco/dto/LoginDto.java
@@ -0,0 +1,11 @@
+package kfq.springcoco.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    private String usernameOrEmail;
+    private String password;
+
+}
---
+++ src/main/java/kfq/springcoco/dto/SignUpDto.java
@@ -0,0 +1,12 @@
+package kfq.springcoco.dto;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+
+    private String email;
+    private String nickname;
+    private String password;
+
+}
