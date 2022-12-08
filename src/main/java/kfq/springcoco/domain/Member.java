88ba9de616dd8a0e4693cbd3786b5976d72d953b
bookpark <bkbkgg@gmail.com>
2022-12-08 11:25:25 +0900

    롬복 생성자 annotation 추가

--- src/main/java/kfq/springcoco/domain/Member.java
+++ src/main/java/kfq/springcoco/domain/Member.java
@@ -1,6 +1,8 @@
 package kfq.springcoco.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,7 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Member {
 
     @Id
