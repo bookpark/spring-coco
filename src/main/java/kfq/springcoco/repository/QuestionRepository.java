70f7a0ac5674cc6c0cb99d6a0448255a1af1517a
bookpark <bkbkgg@gmail.com>
2022-12-08 15:12:42 +0900

    JPA 수정

--- src/main/java/kfq/springcoco/repository/QuestionRepository.java
+++ src/main/java/kfq/springcoco/repository/QuestionRepository.java
@@ -3,5 +3,12 @@ package kfq.springcoco.repository;
 import kfq.springcoco.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findByTitle(String title);
+
+    Question findByTitleAndContent(String title, String content);
+
+    List<Question> findByTitleLike(String title);
 }
