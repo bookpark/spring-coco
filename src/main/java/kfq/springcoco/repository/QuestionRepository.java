8f7899680b525af6f444357f7c5559b1b6bfaef1
bookpark <bkbkgg@gmail.com>
2022-12-09 13:48:25 +0900

    Custom query 삭제

--- src/main/java/kfq/springcoco/repository/QuestionRepository.java
+++ src/main/java/kfq/springcoco/repository/QuestionRepository.java
@@ -6,9 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-    Question findByTitle(String title);
-
-    Question findByTitleAndContent(String title, String content);
-
-    List<Question> findByTitleLike(String title);
 }
