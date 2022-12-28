6f288fb4b67e40b6ba465f43a38fbc1bc93860b7
bookpark <bkbkgg@gmail.com>
2022-12-28 12:58:54 +0900

    멤버별 기술 삭제 api 구현

--- src/main/java/kfq/springcoco/repository/SkillRepository.java
+++ src/main/java/kfq/springcoco/repository/SkillRepository.java
@@ -4,7 +4,5 @@ import kfq.springcoco.entity.Member;
 import kfq.springcoco.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.criteria.CriteriaBuilder;
-
 public interface SkillRepository extends JpaRepository<Skill, Member> {
 }
